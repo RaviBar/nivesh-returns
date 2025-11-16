10b725968736d95677b07be62270a35b01e07ea5
Ravi Baraskar <ravibaraskar108@gmail.com>
2025-11-16 11:30:59 +0530

    fix: typo errror

--- src/pages/UserDashboards/Wallet.jsx
+++ src/pages/UserDashboards/Wallet.jsx
@@ -184,7 +184,7 @@ const WalletPage = () => {
 
           <Card className="h-[106px]">
             <div className="px-6 py-6 flex items-center gap-[22px] h-full">
-              <img src={upgraphIcon} alt="Deposits" className="w-[40px] h-[24px]" />
+              <img src={upGraphIcon} alt="Deposits" className="w-[40px] h-[24px]" />
               <div>
                 <h3 className="text-[#666D80] text-xs mb-[10px]">Total Deposited</h3>
                 <p className="text-xl font-semibold text-white">
